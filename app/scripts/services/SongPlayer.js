bf6c0ff9363f423b4984f7ab26139b9398d6facc
Carolyn Walcott <carolynoliveira@Carolyns-MacBook-Air.local>
2017-06-21 22:51:41 -0400

    Assignment 7: Services: Part 2 - Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -12,7 +12,7 @@
 /**
 * @function setSong
 * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-* @param {Object} song
+* @param {Object} song, the selected song
 */
       var setSong = function(song) {
         if (currentBuzzObject) {
@@ -26,31 +26,44 @@
       });
 
       currentSong = song;
-
     };
+
+/**
+* @function playSong
+* @desc Plays the song selected and sets song playing to true
+* @param {Object} songPlay, the selected song
+*/
+
+    var playSong = function(songPlay) {
+      currentBuzzObject.play();
+      songPlay.playing = true;
+    };
+/**
+* @function SongPlayer.play
+* @desc If the current song is not the same as the selected song, stop the current song and load new audio file,
+* play selected song and set song playing to true
+* @param {Object} song, the selected song
+*/
 
       SongPlayer.play = function(song) {
         if (currentSong !== song) {
           setSong(song);
-        //   if (currentBuzzObject) {
-        //     currentBuzzObject.stop();
-        //     currentSong.playing = null;
-        //   } else if (currentSong === song) {
-        //     if (currentBuzzObject.isPaused()) {
-        //       currentBuzzObject.play();
-        //     }
-        //   }
-        // };
-        // currentBuzzObject = new buzz.sound(song.audioUrl, {
-        //   formats: ['mp3'],
-        //   preload: true
-        // });
-        //
-        // currentSong = song;
-
-        currentBuzzObject.play();
-        song.playing = true;
+          playSong(song);
+          // currentBuzzObject.play();
+          // song.playing = true;
+        } else if (currentSong === song) {
+          if (currentBuzzObject.isPaused()) {
+              // currentBuzzObject.play();
+              playSong(song);
+          }
+        }
       };
+
+/**
+* @function SongPlayer.pause
+* @desc Pauses the selected song and changes song.playing to false
+* @param {Object} song, the selected song
+*/
 
       SongPlayer.pause = function(song) {
         currentBuzzObject.pause();
@@ -58,7 +71,7 @@
       };
 
       return SongPlayer;
-    }
+    };
 
     angular
         .module('blocJams')
